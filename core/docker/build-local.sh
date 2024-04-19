174d4ef538c54251bb478b298c4b041aa6726f26
wangweijie <wangweijie@loongson.cn>
2024-04-19 16:42:28 +0800

    Add loongarch support
    Signed-off-by: wangweijie <wangweijie@loongson.cn>

--- core/docker/build-local.sh
+++ core/docker/build-local.sh
@@ -10,7 +10,7 @@ cd ${SCRIPT_DIR}
 
 # Move to the root directory to run maven for current version.
 pushd ${SOURCE_DIR}
-TRINO_VERSION=$(./mvnw --quiet help:evaluate -Dexpression=project.version -DforceStdout)
+TRINO_VERSION=371
 popd
 
 WORK_DIR="$(mktemp -d)"
@@ -22,18 +22,15 @@ cp -R default ${WORK_DIR}/
 
 cp ${SOURCE_DIR}/client/trino-cli/target/trino-cli-${TRINO_VERSION}-executable.jar ${WORK_DIR}
 
-CONTAINER="trino:${TRINO_VERSION}"
+CONTAINER="cr.loongnix.cn/apache/trino:${TRINO_VERSION}"
 
-docker build ${WORK_DIR} --pull --platform linux/amd64 -f Dockerfile -t ${CONTAINER}-amd64 --build-arg "TRINO_VERSION=${TRINO_VERSION}"
-docker build ${WORK_DIR} --pull --platform linux/arm64 -f Dockerfile -t ${CONTAINER}-arm64 --build-arg "TRINO_VERSION=${TRINO_VERSION}"
+docker build ${WORK_DIR} -f Dockerfile -t ${CONTAINER} --build-arg "TRINO_VERSION=${TRINO_VERSION}"
 
 rm -r ${WORK_DIR}
 
 # Source common testing functions
 . container-test.sh
 
-test_container ${CONTAINER}-amd64 linux/amd64
-test_container ${CONTAINER}-arm64 linux/arm64
+test_container ${CONTAINER} linux/loongarch64
 
-docker image inspect -f '🚀 Built {{.RepoTags}} {{.Id}}' ${CONTAINER}-amd64
-docker image inspect -f '🚀 Built {{.RepoTags}} {{.Id}}' ${CONTAINER}-arm64
+docker image inspect -f '🚀 Built {{.RepoTags}} {{.Id}}' ${CONTAINER}
--- core/trino-main/src/main/java/io/trino/server/TrinoSystemRequirements.java
+++ core/trino-main/src/main/java/io/trino/server/TrinoSystemRequirements.java
@@ -71,13 +71,16 @@ final class TrinoSystemRequirements
         String osName = StandardSystemProperty.OS_NAME.value();
         String osArch = StandardSystemProperty.OS_ARCH.value();
         if ("Linux".equals(osName)) {
-            if (!ImmutableSet.of("amd64", "aarch64", "ppc64le").contains(osArch)) {
-                failRequirement("Trino requires amd64, aarch64, or ppc64le on Linux (found %s)", osArch);
+            if (!ImmutableSet.of("amd64", "aarch64", "ppc64le", "loongarch64", "loong64").contains(osArch)) {
+                failRequirement("Trino requires amd64, aarch64, ppc64le or loongarch64 on Linux (found %s)", osArch);
             }
             if ("aarch64".equals(osArch)) {
                 warnRequirement("Support for the ARM architecture is experimental");
             }
             else if ("ppc64le".equals(osArch)) {
+                warnRequirement("Support for the POWER architecture is experimental");
+            }
+            else if ("loongarch64".equals(osArch) || "loong64".equals(osArch)) {
                 warnRequirement("Support for the POWER architecture is experimental");
             }
         }
